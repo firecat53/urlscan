f97b90e662002132535a0da75e8f0d2288c244c4
Scott Hansen <firecat4153@gmail.com>
2014-11-30 12:01:50 -0800

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name="urlscan",
-      version="0.7.0",
+      version="0.7.2",
       description="View the URLs in an email message",
-      author="Daniel Burrows",
-      author_email="dburrows@debian.org",
+      author="Scott Hansen",
+      author_email="firecat4153@gmail.com",
       packages=['urlscan'],
       scripts=['bin/urlscan'],
-      package_data={'urlscan': ['README.md']},
-      data_files=[('share/doc/urlscan', ['README.md', 'COPYING']),
+      package_data={'urlscan': ['README.rst']},
+      data_files=[('share/doc/urlscan', ['README.rst', 'COPYING']),
                   ('share/man/man1', ['urlscan.1'])],
       license="GPLv2",
       install_requires=["urwid"]
