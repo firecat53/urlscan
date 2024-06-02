1ec81ec801b35069a5bb1205af25b6d0a84d6905
Scott Hansen <tech@firecat53.net>
2024-06-01 21:13:55 -0700

    Housekeeping. Remove accidentally committed conf.py, update flake.lock and tld list, update man page date

--- conf.py
+++
@@ -1,3 +0,0 @@
-URLINTERNALPATTERN = r'[{}()@\w/\\\-%?!&.=:;+,#~]'
-URLTRAILINGPATTERN = r'[{}(@\w/\-%&=+#]'
-HTTPURLPATTERN = (r'(?:(https?|file|ftps?)://' + URLINTERNALPATTERN + r'*' + URLTRAILINGPATTERN + r')')
